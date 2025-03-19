83209f12af8919cf79613ffb33460d8a0d2e96ab
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2025-03-19 12:00:07 +0500

    all templates are ready

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ from .views import index, contact, detail, error_page
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
-    path('detail/', detail, name='detail'),
+    path('detail/<int:pk>', detail, name='detail'),
     path('404/', error_page, name='error-page'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404
 from .models import News, Category
 
 def index(request):
@@ -22,11 +22,71 @@ def index(request):
     }
     return render(request, 'index.html', context=context)
 
-def contact(request):
-    return render(request, 'contact.html')
 
-def detail(request):
-    return render(request, 'single_page.html')
+def contact(request):
+    news = News.objects.all().order_by('-created_at')
+    ctgs = Category.objects.all()
+    latest_news = News.objects.all().order_by('-created_at')
+    news_uzb = News.objects.filter(category_id=1)
+    news_world = News.objects.filter(category_id=2)
+    news_eco = News.objects.filter(category_id=3)
+    news_soc = News.objects.filter(category_id=4)
+    news_sport = News.objects.filter(category_id=5)
+    context = {
+        'news': news,
+        'latest_news': latest_news[2:],
+        'news_uzb': news_uzb,
+        'news_world': news_world,
+        'news_eco': news_eco,
+        'news_soc': news_soc,
+        'news_sport': news_sport,
+        'ctgs': ctgs,
+    }
+    return render(request, 'contact.html', context=context)
+
+
+def detail(request, pk):
+    # info = get_object_or_404(News, id=pk)
+    info = News.objects.get(id=pk)
+    news = News.objects.all().order_by('-created_at')
+    ctgs = Category.objects.all()
+    latest_news = News.objects.all().order_by('-created_at')
+    news_uzb = News.objects.filter(category_id=1)
+    news_world = News.objects.filter(category_id=2)
+    news_eco = News.objects.filter(category_id=3)
+    news_soc = News.objects.filter(category_id=4)
+    news_sport = News.objects.filter(category_id=5)
+    context = {
+        'info': info,
+        'news': news,
+        'latest_news': latest_news[2:],
+        'news_uzb': news_uzb,
+        'news_world': news_world,
+        'news_eco': news_eco,
+        'news_soc': news_soc,
+        'news_sport': news_sport,
+        'ctgs': ctgs,
+    }
+    return render(request, 'single_page.html', context=context)
+
 
 def error_page(request):
-    return render(request, '404.html')
+    news = News.objects.all().order_by('-created_at')
+    ctgs = Category.objects.all()
+    latest_news = News.objects.all().order_by('-created_at')
+    news_uzb = News.objects.filter(category_id=1)
+    news_world = News.objects.filter(category_id=2)
+    news_eco = News.objects.filter(category_id=3)
+    news_soc = News.objects.filter(category_id=4)
+    news_sport = News.objects.filter(category_id=5)
+    context = {
+        'news': news,
+        'latest_news': latest_news[2:],
+        'news_uzb': news_uzb,
+        'news_world': news_world,
+        'news_eco': news_eco,
+        'news_soc': news_soc,
+        'news_sport': news_sport,
+        'ctgs': ctgs,
+    }
+    return render(request, '404.html', context=context)
